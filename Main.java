ef4d048ea0c9e1a3fe83143bbc748e9a8e36c143
Austin Vandegriff <austinvandegriff@yahoo.com>
2026-06-12 15:23:58 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+import classes.farm.Farm;
+public class Main {
+    public static void main(String[] args) { 
+ 	Farm workFarm = new Farm("Ryzebols");
+   		workFarm.Display();
+	 }
+}
---
+++ classes/farm/Farm.java
@@ -0,0 +1,12 @@
+package classes.farm;
+public class Farm {
+	String name;
+	public Farm(String name){
+		this.name = name;
+	}
+
+	public static void Display(){
+		System.out.println("Brookview");
+
+	}
+}
